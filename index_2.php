9af80468eb4bbb07cdd7b7bc73e0183b9b6f0905
Andrus Rinde <andrusrinde@users.noreply.github.com>
2018-09-11 14:22:20 +0300

    lisatud esimene fail

---
+++ index_2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Andrus Rinde, õppetöö</title>
+</head>
+<body>
+  <h1>Andrus Rinde</h1>
+  <p>Siin on minu <a href="http://www.tlu.ee">TLÜ</a> õppetöö raames valminud veebilehed. Need ei oma mingit sügavat sisu ja nende kopeerimine ei oma mõtet.</p>
+  <img src="../tlu_terra_600x400_2.jpg" alt="TLÜ Terra õppehoone">
+  <p>Minu sõber teeb ka <a href="../../../~jaagala">veebi</a></p>
+</body>
+</html>
