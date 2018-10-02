b447755c0cd1804249ed5d6e54ef5ac39a5ae5ef
Andrus Rinde <andrusrinde@users.noreply.github.com>
2018-10-02 13:06:42 +0300

    Lisatud viies tund

--- tund_5/newuser.php
+++ tund_5/newuser.php
@@ -1,13 +1,18 @@
 <?php
   //lisan teise php faili
   require("functions.php");
-  $firstName = "Tundmatu";
-  $lastName = "Kodanik";
-  $fullName = "";
-  $birthMonth = date("m");
-  $birthYear = date("Y") - 15;
-  $birthDay = date("d");
+  $notice = "";
+  $firstName = "";
+  $lastName = "";
+  $birthMonth = null;
+  $birthYear = null;
+  $birthDay = null;
+  $birthDate = "";
+  $gender = null;
+  $email = "";
+  
   $monthNamesET = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni","juuli", "august", "september", "oktoober", "november", "detsember"];
+  
   
   //püüan POST andmed kinni
   //var_dump($_POST);
@@ -24,31 +29,22 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>
-  <?php
-    echo $firstName;
-	echo " ";
-	echo $lastName;
-  ?>
-  , õppetöö</title>
+  <title>Uue kasutaja loomine</title>
 </head>
 <body>
-  <h1>
-  <?php
-    echo $firstName ." " .$lastName;
-  ?>
-  </h1>
+  <h1>Loo kasutaja</h1>
   <p>Siin on minu <a href="http://www.tlu.ee">TLÜ</a> õppetöö raames valminud veebilehed. Need ei oma mingit sügavat sisu ja nende kopeerimine ei oma mõtet.</p>
   <hr>
   
   <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    <label>Eesnimi: </label>
-    <input type="text" name="firstname">
-    <label>Perekonnanimi: </label>
-    <input type="text" name="lastname">
+    <label>Eesnimi: </label><br>
+    <input type="text" name="firstname"><br>
+    <label>Perekonnanimi: </label><br>
+    <input type="text" name="lastname"><br>
 	<label>Sünnipäev: </label>
 	  <?php
 	    echo '<select name="birthDay">' ."\n";
+		echo '<option value="" selected disabled>Päev</option>' ."\n";
 		for ($i = 1; $i < 32; $i ++){
 			echo '<option value="' .$i .'"';
 			if ($i == $birthDay){
@@ -61,6 +57,7 @@
 	  <label>Sünnikuu: </label>
 	  <?php
 	    echo '<select name="birthMonth">' ."\n";
+		echo '<option value="" selected disabled>Kuu</option>' ."\n";
 		for ($i = 1; $i < 13; $i ++){
 			echo '<option value="' .$i .'"';
 			if ($i == $birthMonth){
@@ -74,7 +71,8 @@
 	  <!--<input name="birthYear" type="number" min="1914" max="2003" value="1998">-->
 	  <?php
 	    echo '<select name="birthYear">' ."\n";
-		for ($i = $birthYear; $i >= date("Y") - 100; $i --){
+		echo '<option value="" selected disabled>Aasta</option>' ."\n";
+		for ($i = date("Y") - 15; $i >= date("Y") - 100; $i --){
 			echo '<option value="' .$i .'"';
 			if ($i == $birthYear){
 				echo " selected ";
@@ -83,6 +81,19 @@
 		}
 		echo "</select> \n";
 	  ?>
+	  <br>
+	  <label>Sugu:</label><br>
+	  <input name="gender" type="radio" value="2"><label>Naine</label>
+	  <br>
+	  <input name="gender" type="radio" value="1"><label>Mees</label>
+	  <br>
+	  <label>E-postiaadress (kasutajatunnuseks): </label><br>
+      <input type="email" name="email"><br>
+      <label>Salasõna (min 8 märki): </label><br>
+      <input type="password" name="password"><br>
+	  
+	  <input type="submit" name="submitUserData" value="Loo kasutaja">
+  </form>
   
 </body>
 </html>
